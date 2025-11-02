5c298e150c4627310e5ba2a2e8b357db6b50ba6d
Sajid Ahmed <sajid_640@hotmail.com>
2025-11-02 16:46:29 +0000

    apply auto-fixes

--- gdp/visualisation.py
+++ gdp/visualisation.py
@@ -1,9 +1,12 @@
 import seaborn as sns
+import pandas as pd
+import matplotlib
 
 
-def plot_gdp(df):
+def plot_gdp(df: pd.DataFrame) -> matplotlib.axes.Axes:
     """
-    This function takes in a dataframe and plots the GDP of the countries in the dataframe.
+    This function takes in a dataframe and plots the GDP of the countries
+    in the dataframe.
     """
 
     return sns.lineplot(data=df, x="Year", y="GDP", hue="Country Name")
